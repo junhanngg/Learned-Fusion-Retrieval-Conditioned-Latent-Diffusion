a7e0e0b5acdef787c68392c4450e916e3c2823b6
jun <junhanng6@gmail.com>
2026-04-27 14:23:00 +0100

    modularised vae

--- models/vae.py
+++ models/vae.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from torch.optim import AdamW
 from tqdm.auto import tqdm
 from torch.optim.lr_scheduler import ReduceLROnPlateau, CosineAnnealingLR
 
